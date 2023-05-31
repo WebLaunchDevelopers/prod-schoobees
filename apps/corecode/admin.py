063b502a33dee754ad93fc36f20531ee35ad03f3
Gummadi Avinash <avinashgummadi2021weblaunch@gmail.com>
2023-06-01 01:04:38 +0530

    resolved api issues

--- apps/corecode/admin.py
+++ apps/corecode/admin.py
@@ -8,7 +8,7 @@ from .models import (
     Calendar,
     Driver,
     Route,
-    RouteNodes
+    RouteNode
 )
 
 class SiteConfigAdmin(admin.ModelAdmin):
@@ -45,14 +45,14 @@ class RouteAdmin(admin.ModelAdmin):
     list_filter = ('user',)
     search_fields = ('name', 'user__username')
 
-class RouteNodesAdmin(admin.ModelAdmin):
+class RouteNodeAdmin(admin.ModelAdmin):
     list_display = ('area', 'route', 'is_start_stop', 'is_destination_stop')
     list_filter = ('route',)
     search_fields = ('area', 'route__name')
 
 admin.site.register(Driver, DriverAdmin)
 admin.site.register(Route, RouteAdmin)
-admin.site.register(RouteNodes, RouteNodesAdmin)
+admin.site.register(RouteNode, RouteNodeAdmin)
 admin.site.register(SiteConfig, SiteConfigAdmin)
 admin.site.register(AcademicSession, AcademicSessionAdmin)
 admin.site.register(AcademicTerm, AcademicTermAdmin)
