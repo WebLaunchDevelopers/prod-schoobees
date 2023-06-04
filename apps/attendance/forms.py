e69de53211d5dbc1f4f9c3f246bb4b3fea21b8e6
Gummadi Avinash <avinashgummadi2021weblaunch@gmail.com>
2023-06-04 23:59:21 +0530

    apis reframing and bugs resolved

--- apps/attendance/forms.py
+++ apps/attendance/forms.py
@@ -1,17 +1,11 @@
 from django import forms
-from django.forms import modelformset_factory
+from django.forms import BaseModelFormSet, Select, modelformset_factory
 from apps.base.models import CustomUser
 from apps.corecode.models import AcademicSession, AcademicTerm, Subject
-from apps.corecode.models import (
-    AcademicSession,
-    AcademicTerm,
-    StudentClass,
-    Subject,
-)
+from apps.corecode.models import AcademicSession, AcademicTerm, StudentClass, Subject
 from .models import Attendance
 
 ATTENDANCE_CHOICES = [
-    ('', 'Select'),
     ('Present', 'Present'),
     ('Absent', 'Absent'),
     ('Late', 'Late'),
@@ -20,8 +14,8 @@ ATTENDANCE_CHOICES = [
 ]
 
 class UpdateAttendance(forms.Form):
-    subjects = forms.ModelChoiceField(empty_label='Select Subject',queryset=Subject.objects.all())
-    class_name = forms.ModelChoiceField(empty_label='Select Class',queryset=StudentClass.objects.all())
+    subjects = forms.ModelChoiceField(empty_label='Select Subject', queryset=Subject.objects.all())
+    class_name = forms.ModelChoiceField(empty_label='Select Class', queryset=StudentClass.objects.all())
     date_of_attendance = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
 
     def __init__(self, *args, user=None, **kwargs):
@@ -30,8 +24,21 @@ class UpdateAttendance(forms.Form):
             self.fields['class_name'].queryset = StudentClass.objects.filter(user=user)
             self.fields['subjects'].queryset = Subject.objects.filter(user=user)
 
+class BaseAttendanceFormSet(BaseModelFormSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for form in self.forms:
+            form.fields['attendance_status'].empty_label = None
+            form.fields['attendance_status'].widget = Select(
+                choices=ATTENDANCE_CHOICES,
+                attrs={'required': 'required'},
+            )
+            form.fields['attendance_status'].initial = 'Present'
 
 EditAttendance = modelformset_factory(
-    Attendance, fields=("id", "attendance_status"), extra=0, can_delete=True,
-    widgets={"attendance_status": forms.Select(choices=ATTENDANCE_CHOICES, attrs={'required': 'required'})}
+    Attendance,
+    formset=BaseAttendanceFormSet,
+    fields=("id", "attendance_status"),
+    extra=0,
+    can_delete=True,
 )
